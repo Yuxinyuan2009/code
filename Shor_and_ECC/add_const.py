8d8a47f1b3f163dc3e924145959fa7f12a976054
Xinyuan Yu <1993133029@qq.com>
2025-08-18 10:22:41 +0800

    add cmod_add

--- Shor_and_ECC/add_const.py
+++ Shor_and_ECC/add_const.py
@@ -26,3 +26,14 @@ def cadd_const(p, x, t):
                 c.cphase(p, x[i], theta=np.pi / (2 ** (i + j - n + 1)))
     c.append(QFT(x).inverse())
     return c
+
+def ccadd_const(p, q, x, t):
+    c = tc.Circuit(max(x + [p, q]) + 1)
+    n = len(x)
+    c.append(QFT(x))
+    for j in range(n):
+        if t>>(3 - j) & 1:
+            for i in range(n - 1 - j, n):
+                c.append(ccphase(p, q, x[i], theta=np.pi / (2 ** (i + j - n + 1))))
+    c.append(QFT(x).inverse())
+    return c
--- Shor_and_ECC/mod_add.py
+++ Shor_and_ECC/mod_add.py
@@ -29,3 +29,26 @@ c = tc.Circuit(9)
 c.append(mod_add(list(range(4)), list(range(4, 8)), [8]))
 c.draw(output="mpl").savefig("mod_add.png")
 '''
+def cmod_add(p, x, y, z):
+    '''
+    p: control qubit z[0]: ancilla qubit
+    '''
+    c = tc.Circuit(max(x + y + z + [p]) + 2)
+    c.append(cadd(p, x, y))
+    c.append(add_const(y, 9))
+    c.toffoli(p, y[0], z[0])
+    c.append(ccadd_const(p, z[0], y, 7))
+    c.append(cadd(p, x, y).inverse())
+    c.x(y[0])
+    c.toffoli(p, y[0], z[0])
+    c.x(y[0])
+    c.append(cadd(p, x, y))
+    return c
+'''
+c = tc.Circuit(10)
+c.x(1)
+c.append(set(6, [2, 3, 4, 5]))
+c.append(set(5, [6, 7, 8, 9]))
+c.append(cmod_add(1, [2, 3, 4, 5], [6, 7, 8, 9], [0]))
+out(c.state())
+'''
